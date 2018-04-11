033eceac25ed893ddaa803955336ee48493d8ab8
Jan Grądzki <gradzki.jan@gmail.com>
2018-04-11 17:17:34 +0200

    Implemented Main and AcceptButton components tests.

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,10 +13,6 @@ module.exports = {
         test: /.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react', 'stage-2'],
-          plugins: ['transform-object-rest-spread'],
-        },
       },
       {
         test: /\.html$/,
