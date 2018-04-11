c2e30a9afa4c486d3c431959564827b9da9b968d
Jan Grądzki <gradzki.jan@gmail.com>
2018-04-11 11:48:43 +0200

    Implemented background opacity.

--- src/components/AcceptButton/AcceptButton.jsx
+++ src/components/AcceptButton/AcceptButton.jsx
@@ -30,9 +30,34 @@ class AcceptButton extends Component {
 		const color = find(colors, color => color.name === input);
 
 		if (color && color.hex) {
-			document.body.style.backgroundColor = `#${color.hex}`;
+			const rgb = this._hexToRgb(color.hex);
+			const backgroundColor = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.5)`;
+
+			document.body.style.backgroundColor = backgroundColor;
 		}
 	}
+
+	/**
+	 * converts hex to rgb
+	 * @param {string} hex
+	 * @return {object}
+	 */
+	_hexToRgb(hex) {
+		const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+
+		hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+			return r + r + g + g + b + b;
+		});
+
+		const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16),
+		} : null;
+	}
+
 
 	static propTypes = {
 		colors: PropTypes.arrayOf(PropTypes.shape({
